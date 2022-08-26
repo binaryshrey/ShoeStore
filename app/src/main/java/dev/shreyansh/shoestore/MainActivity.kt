eecc8aa91e83a329391d13d36f959745fce80d2c
binaryshrey <shreyansh.saurabh2016@vitstudent.ac.in>
2022-08-27 03:28:52 +0530

    applies dataBinding to mainActivity

--- app/src/main/java/dev/shreyansh/shoestore/MainActivity.kt
+++ app/src/main/java/dev/shreyansh/shoestore/MainActivity.kt
@@ -2,10 +2,15 @@ package dev.shreyansh.shoestore
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import dev.shreyansh.shoestore.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
     }
 }
