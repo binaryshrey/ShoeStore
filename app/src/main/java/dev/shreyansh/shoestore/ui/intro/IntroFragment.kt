322b591fc0059b22362dc039384aa73d49b3b479
binaryshrey <shreyansh.saurabh2016@vitstudent.ac.in>
2022-08-27 04:34:35 +0530

    updates introFragment

--- app/src/main/java/dev/shreyansh/shoestore/ui/intro/IntroFragment.kt
+++ app/src/main/java/dev/shreyansh/shoestore/ui/intro/IntroFragment.kt
@@ -5,16 +5,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import dev.shreyansh.shoestore.R
+import dev.shreyansh.shoestore.databinding.FragmentIntroBinding
 
 
 class IntroFragment : Fragment() {
+
+    private lateinit var binding: FragmentIntroBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_intro, container, false)
+
+        (activity as AppCompatActivity).supportActionBar?.hide()
+        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_intro, container, false)
+        return binding.root
     }
 }
