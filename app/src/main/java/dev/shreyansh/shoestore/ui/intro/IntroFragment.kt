753d4d5fe0164ec532d026bb5a8f585ceb9acb0e
binaryshrey <shreyansh.saurabh2016@vitstudent.ac.in>
2022-08-27 12:26:12 +0530

    adds signUpFrag

--- app/src/main/java/dev/shreyansh/shoestore/ui/intro/IntroFragment.kt
+++ app/src/main/java/dev/shreyansh/shoestore/ui/intro/IntroFragment.kt
@@ -7,13 +7,20 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment.Companion.findNavController
+import androidx.navigation.fragment.findNavController
 import dev.shreyansh.shoestore.R
 import dev.shreyansh.shoestore.databinding.FragmentIntroBinding
+import dev.shreyansh.shoestore.viewmodel.ShoeViewModel
 
 
 class IntroFragment : Fragment() {
 
     private lateinit var binding: FragmentIntroBinding
+    private val viewModel: ShoeViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,6 +29,15 @@ class IntroFragment : Fragment() {
 
         (activity as AppCompatActivity).supportActionBar?.hide()
         binding = DataBindingUtil.inflate(inflater,R.layout.fragment_intro, container, false)
+        binding.viewModel = viewModel
+
+        viewModel.getStarted.observe(viewLifecycleOwner) { hasStarted ->
+            if (hasStarted) {
+                viewModel.onGetStartedComplete()
+                findNavController().navigate(R.id.action_introFragment_to_signupFragment)
+            }
+        }
+
         return binding.root
     }
 }
