15dee1280abb4f08e0a42fe5f82b7f5a9f87a89b
binaryshrey <shreyansh.saurabh2016@vitstudent.ac.in>
2022-08-27 14:00:38 +0530

    updates introOnboarding view

---
+++ app/src/main/java/dev/shreyansh/shoestore/ui/onboarding/IntroOnboardingFragment.kt
@@ -0,0 +1,21 @@
+package dev.shreyansh.shoestore.ui.onboarding
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import dev.shreyansh.shoestore.R
+
+class IntroOnboardingFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_intro_onboarding, container, false)
+    }
+
+}
