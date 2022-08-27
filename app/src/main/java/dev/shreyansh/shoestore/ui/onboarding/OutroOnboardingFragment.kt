09db269d7b69850fdf219298f44bdfae0af11aad
binaryshrey <shreyansh.saurabh2016@vitstudent.ac.in>
2022-08-27 14:20:09 +0530

    updates outroOnboarding view

---
+++ app/src/main/java/dev/shreyansh/shoestore/ui/onboarding/OutroOnboardingFragment.kt
@@ -0,0 +1,24 @@
+package dev.shreyansh.shoestore.ui.onboarding
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import dev.shreyansh.shoestore.R
+
+
+class OutroOnboardingFragment : Fragment() {
+
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_outro_onboarding, container, false)
+    }
+
+
+}
